ffdd420c93f3f7a9645b992c2ca7825a75ed58de
Maximo <maximoluzkevich@gmail.com>
2026-06-06 02:03:06 -0300

    cree RolController

---
+++ AutoRent/src/main/java/com/AutoRent/Backend/controller/RolController.java
@@ -0,0 +1,32 @@
+package com.AutoRent.Backend.controller;
+
+import com.AutoRent.Backend.model.Rol;
+import com.AutoRent.Backend.model.enums.NombreRol;
+import com.AutoRent.Backend.service.RolService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RolController {
+
+    private final RolService rolService;
+
+    public RolController(RolService rolService) {
+        this.rolService = rolService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Rol>> listarRoles() {
+        return ResponseEntity.ok(rolService.listarRoles());
+    }
+
+    @GetMapping("/{nombreRol}")
+    public ResponseEntity<Rol> buscarPorNombre(@PathVariable NombreRol nombreRol) {
+        return ResponseEntity.ok(rolService.buscarPorNombre(nombreRol));
+    }
+}
